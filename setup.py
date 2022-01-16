070493e0fb5e3fb5642ec9b682512d44fc4ddc97
Shakshi3104 <pikanchisato@yahoo.co.jp>
2022-01-16 15:56:21 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+setuptools.setup(
+    name="tfmars",
+    version="0.3.6",
+    author="Shakshi3104",
+    description="MarNASNet and famous CNN models for Sensor-based Human Activity Recognition",
+    url="https://github.com/Shakshi3104/tfmars",
+    packages=setuptools.find_packages(where='src'),
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+    python_requires=">=3.6, <4",
+    package_dir={'': 'src'},
+)
