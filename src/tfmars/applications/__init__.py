63b7ed88c7a108448328c9d32cad7a8f279b8e58
Shakshi3104 <pikanchisato@yahoo.co.jp>
2022-01-16 16:02:56 +0900

    bug fix

--- src/tfmars/applications/__init__.py
+++ src/tfmars/applications/__init__.py
@@ -1,19 +1,19 @@
-from vgg16 import VGG16, VGG16WithAttention
-from xception import Xception, XceptionWithAttention
-from simplecnn import SimpleCNN, SimpleCNNWithAttention
-from resnet import ResNet18, ResNet18WithAttention
-from pyramidnet import PyramidNet18, PyramidNet18WithAttention
-from nasnet import NASNetMobile, NASNetMobileWithAttention
-from mobilenet_v3 import MobileNetV3Small, MobileNetV3SmallWithAttention
-from mobilenet_v2 import MobileNetV2, MobileNetV2WithAttention
-from mobilenet import MobileNet, MobileNetWithAttention
-from mnasnet import MnasNet, MnasNetWithAttention
-from inception_v3 import InceptionV3, InceptionV3WithAttention
-from efficientnet_lite import EfficientNetLite0, EfficientNetLite0WithAttention
-from efficientnet import EfficientNetB0, EfficientNetB0WithAttention
-from densenet import DenseNet121, DenseNet121WithAttention
+from .vgg16 import VGG16, VGG16WithAttention
+from .xception import Xception, XceptionWithAttention
+from .simplecnn import SimpleCNN, SimpleCNNWithAttention
+from .resnet import ResNet18, ResNet18WithAttention
+from .pyramidnet import PyramidNet18, PyramidNet18WithAttention
+from .nasnet import NASNetMobile, NASNetMobileWithAttention
+from .mobilenet_v3 import MobileNetV3Small, MobileNetV3SmallWithAttention
+from .mobilenet_v2 import MobileNetV2, MobileNetV2WithAttention
+from .mobilenet import MobileNet, MobileNetWithAttention
+from .mnasnet import MnasNet, MnasNetWithAttention
+from .inception_v3 import InceptionV3, InceptionV3WithAttention
+from .efficientnet_lite import EfficientNetLite0, EfficientNetLite0WithAttention
+from .efficientnet import EfficientNetB0, EfficientNetB0WithAttention
+from .densenet import DenseNet121, DenseNet121WithAttention
 
-from marnasnet.marnasnet_c import MarNASNetC
+from .marnasnet.marnasnet_c import MarNASNetC
 
 __all__ = [
     "VGG16", "VGG16WithAttention",
